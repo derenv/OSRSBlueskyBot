e1ec140d0fe4bcccee8e0ccca52abb59dee647c9
Deren Vural <35734401+derenv@users.noreply.github.com>
2024-11-24 10:58:20 +0000

    new entrypoint script

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2024 Deren Vural derenvural@outlook.com
+# SPDX-License-Identifier: MIT
+
+source /usr/src/app/osrs-bluesky-bot/bin/activate && exec python main.py"
